70426539137e5bd7781fc347afa103db99b3c497
GuiDohms <guilherme.dohms@acad.pucrs.br>
2017-05-18 20:26:06 -0300

    a

---
+++ src/Contact.java
@@ -0,0 +1,33 @@
+
+public class Contact {
+	private String name, phoneNum;
+	
+	public Contact(String name, String phoneNum){
+		this.name = name;
+		this.phoneNum = phoneNum;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhoneNum() {
+		return phoneNum;
+	}
+
+	public void setPhoneNum(String phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+
+	@Override
+	public String toString() {
+		return "Contact: " + name + '\n' +
+			   "phoneNum: " + phoneNum;
+	}
+	
+	
+}
