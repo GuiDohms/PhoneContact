df94f169fac40bf2059808d7f8a93fbaad267fbe
GuiDohms <guilherme.dohms@acad.pucrs.br>
2017-05-23 20:48:59 -0300

    a

--- src/Business/ContactList.java
+++ src/Business/ContactList.java
@@ -1,6 +1,42 @@
 package Business;
 
-import 
+import java.util.*;
+
 public class ContactList {
-	private ArrayList
+	private ArrayList<Contact> contactList;
+	
+	public ContactList(){
+		ArrayList<Contact> contactList = new ArrayList<Contact>();
+	}
+	
+	public boolean addContact(Contact contact){
+		if(contact.equals(contactList.equals(contact))){
+			return false;
+		}else{
+			contactList.add(contact);
+		}
+		return true;
+	}
+	
+	public boolean removeContact(Contact contact){
+		if(contact.equals(contactList.equals(contact))){
+			contactList.remove(contact);
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	public void alfabeticListing(){
+		
+	}
+	
+	public Contact findByName(Contact contact){
+		return contact;
+	}
+	
+	
+	
+	
+	
 }
